009abd50d4bcacb68ab648b8c015ddeca9e02d5f
Cem Karakurt <ickarakurt@gmail.com>
2020-01-28 17:45:56 +0300

    cookie accepted

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,32 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import './css/tailwind.css';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showCookieText: true,
+    };
+  }
+
+  render() {
+    const { showCookieText } = this.state;
+    return (
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+          <div style={{ display: showCookieText ? 'flex' : 'none' }} className="w-full bg-white border border-gray-300 fixed bottom-0 flex justify-between items-center text-lg px-2">
+This site uses cookies to analyse traffic, remember your preferences, and optimise your experience.
+            <button type="button" onClick={() => this.setState({ showCookieText: false })} className="bg-countly-700 hover:bg-countly-800 text-white font-bold py-2 px-4">Close</button>
+          </div>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
