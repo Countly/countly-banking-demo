f50bb1e1985c7bddceee7de34e3f7c7faa64ca17
Cem Karakurt <ickarakurt@gmail.com>
2020-01-30 12:11:56 +0300

    18n path for prod

--- src/i18n.js
+++ src/i18n.js
@@ -9,7 +9,7 @@ i18n
     lng: 'en',
     backend: {
       /* translation file path */
-      loadPath: '/assets/i18n/{{ns}}/{{lng}}.json',
+      loadPath: '/countly-banking-demo/assets/i18n/{{ns}}/{{lng}}.json',
     },
     fallbackLng: 'en',
     debug: false,
