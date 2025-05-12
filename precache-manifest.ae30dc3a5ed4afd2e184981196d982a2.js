4006235f3c61ade3ed92f55c246eb6a45f182439
Arif Burak Demiray <57103426+arifBurakDemiray@users.noreply.github.com>
2025-05-12 08:01:14 +0000

    feat: clear id deploy (#13)

---
+++ precache-manifest.ae30dc3a5ed4afd2e184981196d982a2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7146e524ba86ae843ab9d27e47eee429",
+    "url": "https://countly.github.io/countly-banking-demo/index.html"
+  },
+  {
+    "revision": "d3c6494fe4a3ebaf225e",
+    "url": "https://countly.github.io/countly-banking-demo/static/css/2.b4f7f218.chunk.css"
+  },
+  {
+    "revision": "b6d672248e7e8f522202",
+    "url": "https://countly.github.io/countly-banking-demo/static/css/main.09a9b22e.chunk.css"
+  },
+  {
+    "revision": "d3c6494fe4a3ebaf225e",
+    "url": "https://countly.github.io/countly-banking-demo/static/js/2.e2ae3340.chunk.js"
+  },
+  {
+    "revision": "fd64d766fb7e6d1a18e038d229e55314",
+    "url": "https://countly.github.io/countly-banking-demo/static/js/2.e2ae3340.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b6d672248e7e8f522202",
+    "url": "https://countly.github.io/countly-banking-demo/static/js/main.bea0526d.chunk.js"
+  },
+  {
+    "revision": "446a19cf7c185070f733",
+    "url": "https://countly.github.io/countly-banking-demo/static/js/runtime-main.e0879aaf.js"
+  },
+  {
+    "revision": "4c2407bb2bec8fd1c058187733357f9d",
+    "url": "https://countly.github.io/countly-banking-demo/static/media/account.4c2407bb.svg"
+  },
+  {
+    "revision": "15e4ed93a7944957837bf437d8ecd486",
+    "url": "https://countly.github.io/countly-banking-demo/static/media/countly-overview.15e4ed93.png"
+  },
+  {
+    "revision": "7cddf2294c1bb46c751daab05f941468",
+    "url": "https://countly.github.io/countly-banking-demo/static/media/countly.7cddf229.png"
+  },
+  {
+    "revision": "ed52f187b2c4aad7fd02765c9cdc2f92",
+    "url": "https://countly.github.io/countly-banking-demo/static/media/customer.ed52f187.svg"
+  },
+  {
+    "revision": "cbeab2705cd1a8c5d774eac8ed9c9619",
+    "url": "https://countly.github.io/countly-banking-demo/static/media/loan.cbeab270.svg"
+  },
+  {
+    "revision": "662f393586bc273b3d7613add697b3f5",
+    "url": "https://countly.github.io/countly-banking-demo/static/media/saving.662f3935.svg"
+  }
+]);
